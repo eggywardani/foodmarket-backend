0a9816655ec1b46006121d945abba51f87e91c8c
eggywardani <eggyandikawardani@gmail.com>
2021-06-27 01:47:58 +0700

    fix error route

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return redirect()->route('dashboard');
-});
+//Route::get('/', function () {
+//    return redirect()->route('dashboard');
+//});
 
+Route::get('/', function () {
+    return view('dashboard');
+});
 
 Route::prefix('dashboard')
     ->middleware(['auth:sanctum', 'admin'])
